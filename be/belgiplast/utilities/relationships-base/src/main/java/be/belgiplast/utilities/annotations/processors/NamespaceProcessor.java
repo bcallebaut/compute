1c84025b5204abe3a6d40e57f21b87cd19fbe2a7
benoit <benoit.callebaut@gmail.com>
2018-07-01 22:25:28 +0200

    Annotation processor implementation

--- be/belgiplast/utilities/relationships-base/src/main/java/be/belgiplast/utilities/annotations/processors/NamespaceProcessor.java
+++ be/belgiplast/utilities/relationships-base/src/main/java/be/belgiplast/utilities/annotations/processors/NamespaceProcessor.java
@@ -7,10 +7,14 @@ package be.belgiplast.utilities.annotations.processors;
 
 import be.belgiplast.utilities.annotations.Name;
 import be.belgiplast.utilities.annotations.Namespace;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
@@ -23,6 +27,7 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
+import javax.tools.JavaFileObject;
 
 /**
  *
@@ -85,8 +90,27 @@ public class NamespaceProcessor extends AbstractProcessor {
         return true;
     }
 
+    private String createClassname(NamespaceDef def){
+        StringBuilder builder = new StringBuilder();
+        NamespaceDef current = def;
+        while (current != null){
+            builder = builder.insert(0, current.getName());
+            current = current.getParent();
+            if (current != null)
+                builder.insert(0,"/");
+        }
+        return builder.toString();
+    }
+    
     private void generateCode(Map<String, NamespaceDef> nss) {
-        
+        for (NamespaceDef def : nss.values()){
+            try {
+                JavaFileObject fo = filer.createSourceFile(createClassname(def));
+                Writer wr = fo.openWriter();
+            } catch (IOException ex) {
+                Logger.getLogger(NamespaceProcessor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
 }
