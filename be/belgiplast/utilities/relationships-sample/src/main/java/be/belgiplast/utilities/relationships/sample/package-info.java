123d5e194bd7878db0343dcd03c77c96a3d6c7a0
T0194671 <benoit.callebaut@be.thalesgroup.com>
2018-07-12 14:19:44 +0200

    Test project for annotations

---
+++ be/belgiplast/utilities/relationships-sample/src/main/java/be/belgiplast/utilities/relationships/sample/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@Namespace(name="test",names={@Name(name="Foo"),@Name(name="Bar")})
+@Entity(type="Foo")        
+@Entity(type="Foo")
+@Relationship(type="Bar")
+package be.belgiplast.utilities.relationships.sample;
+
+import be.belgiplast.utilities.annotations.Entity;
+import be.belgiplast.utilities.annotations.Name;
+import be.belgiplast.utilities.annotations.Namespace;
+import be.belgiplast.utilities.annotations.Relationship;
+
