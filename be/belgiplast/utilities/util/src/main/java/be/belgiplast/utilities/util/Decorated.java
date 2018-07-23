0ede57736b91f2ead31d11d7e1188bf6b616b377
benoit <benoit.callebaut@gmail.com>
2018-07-23 21:14:02 +0200

    Added Decorator interfaces

---
+++ be/belgiplast/utilities/util/src/main/java/be/belgiplast/utilities/util/Decorated.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.belgiplast.utilities.util;
+
+/**
+ *
+ * @author benoit
+ */
+public interface Decorated {
+    Decorator getDecorator(Class<Decorator> clazz);
+}
